79ffe2bc37f979dc2fa62c1bab0d7dbbbd6d3963
Babarikin <133123656+Babarikin@users.noreply.github.com>
2023-05-10 16:40:56 +0300

    Add files via upload

---
+++ cursach/MBcursachClient/MBcursachClient/MainWindow.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MBcursachClient.ServiceChat;
+
+namespace MBcursachClient
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IServiceChatCallback
+    {
+        bool isConnected = false;
+        ServiceChatClient client;
+        int ID;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        void ConnectUser()
+        {
+            if (!isConnected)
+            {
+                if(tbUserName.Text == "Имя сотрудника") 
+                {
+                    MessageBox.Show("Введите имя");
+                }
+                else
+                {
+                client = new ServiceChatClient(new System.ServiceModel.InstanceContext(this));
+                ID = client.Connect(tbUserName.Text);
+                tbUserName.IsEnabled = false;
+                btnName.Content = "Отключиться";
+                isConnected = true;
+                }
+            }
+        }
+        void DisconnectUser()
+        {
+            if (isConnected)
+            {
+                client.Disconnect(ID);
+                client = null;
+                tbUserName.IsEnabled = true;
+                btnName.Content = "Подключиться";
+                isConnected = false;
+            }
+        }
+
+        public void Show(string Content)
+        {
+            lbtable.Items.Add(Content);
+            lbtable.ScrollIntoView(lbChat.Items[lbChat.Items.Count - 1]);
+        }
+
+        private void btnName_Click(object sender, RoutedEventArgs e)
+        {
+            if (isConnected)
+            {
+                DisconnectUser();
+            }
+            else
+            {
+                ConnectUser();
+            }
+        }
+
+        public void MsgCallback(string msg)
+        {
+            lbChat.Items.Add(msg);
+            lbChat.ScrollIntoView(lbChat.Items[lbChat.Items.Count-1]);
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            DisconnectUser();
+        }
+
+        private void lbMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (client != null)
+                {
+                    client.SendMsg(lbMessage.Text, ID);
+                    lbMessage.Text = String.Empty;
+                }
+            }
+        }
+
+        private void tbUserName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        //string commandText;
+        //string connectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=VOKCMR.mdb";
+        //OleDbConnection conn = null;
+        //Exception exep = null;
+        private void btnShow_Click(object sender, RoutedEventArgs e)
+        {
+            string txt = "";
+            if (client != null)
+            {
+                client.ConnectDB(txt);
+            }
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            if (client != null)
+            {
+                client.SendMsg(lbMessage.Text, ID);
+                lbMessage.Text = String.Empty;
+            }
+        }
+    }
+}
---
+++ cursach/MBcursachWCF/MBcursachWCF/ServiceChat.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MBcursachWCF
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "ServiceChat"
+    // в коде и файле конфигурации.
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class ServiceChat : IServiceChat
+    {
+        List<ServerUser> users = new List<ServerUser>();
+        int nextId = 1;
+
+
+        string commandText;
+        string connectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=VOKCMR.mdb";
+        OleDbConnection conn = null;
+        Exception exep = null;
+
+        public int Connect(string name)
+        {
+            ServerUser user = new ServerUser()
+            {
+                ID = nextId,
+                Name = name,
+                operationContext = OperationContext.Current
+            };
+            nextId++;
+
+            SendMsg(": "+user.Name + " подключился к чату",0);
+            users.Add(user);
+            return user.ID;
+        }
+
+        public void Disconnect(int id)
+        {
+            var user = users.FirstOrDefault(x => x.ID == id);
+            if (user != null)
+            {
+                users.Remove(user);
+                SendMsg(": " + user.Name + " покуинул к чат",0);
+            }
+        }
+
+        public void SendMsg(string msg, int id)
+        {
+            foreach (var item in users)
+            {
+                string answer = DateTime.Now.ToShortTimeString();
+                var user = users.FirstOrDefault(x => x.ID == id);
+                if (user != null)
+                {
+                    answer+=": "+ user.Name+" :";
+                }
+                answer+=msg;
+
+                item.operationContext.GetCallbackChannel<IServerChatCallback>().MsgCallback(answer);
+            }
+        }
+        public void ConnectDB(string msg)
+        {
+            string txt = "";
+            conn = new OleDbConnection(connectionString);
+            conn.Open();
+            string query = "SELECT * FROM procedures";
+            OleDbCommand command = new OleDbCommand(query, conn);
+            //label2.Text = command.ExecuteScalar().ToString();//1 результ
+            OleDbDataReader reader = command.ExecuteReader();//много всего
+            //listBox1.Items.Clear();
+            while (reader.Read())
+            {
+                txt = reader[0].ToString() + ": " + reader[1].ToString() +" ";
+            }
+
+            reader.Close();
+            //SendMsg(": " + txt + " ???", 0);
+            foreach (var item in users)
+            {
+                item.operationContext.GetCallbackChannel<IServerChatCallback>().Show(txt);
+            }
+        }
+    }
+}
